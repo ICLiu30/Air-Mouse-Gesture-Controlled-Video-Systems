02b8f03a2da961c9717722c74d32f6dc36c4673f
I-Cheng Liu <137127399+ICLiu30@users.noreply.github.com>
2023-08-14 23:50:26 -0700

    Update web.py

--- web.py
+++ web.py
@@ -17,11 +17,11 @@ st.write("Enter the **Air Mouse & Gesture-Controlled Video System**. With a mere
 st.write("### Air Mouse")
 st.write("For the Air Mouse system, I harnessed MediaPipe, OpenCV, and PyAutoGUI to detect finger movements, enabling actions like scrolling, clicking, and cursor movement. Additionally, I integrated speech recognition for hands-free text input.")
 
-st.image(mouse)
+st.image(str(mouse))
 st.write("### Gesture-Controlled Video")
 st.write("The Gesture-Controlled Video system is rooted in a Random Forest model. This model deciphers hand gestures to execute video commands, such as fast-forwarding, rewinding, pausing, or adjusting volume, making video navigation seamless and intuitive.")
 
-st.image(video)
+st.image(str(video))
 
 st.write("### Acknowledgements & Further Exploration")
 st.markdown("I'd like to extend my heartfelt thanks to [Murtaza's Workshop](https://www.youtube.com/watch?v=8gPONnGIPgw&t=321) for being the foundational guide during the construction of these systems. For those intrigued by the code and training aspects of this project, I invite you to dive deeper into my [GitHub](https://github.com/ICLiu30/Air-Mouse-Gesture-Controlled-Video-Systems).")
